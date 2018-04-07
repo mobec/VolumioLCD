4a723c8291840bd441ad34587ac91d71e42f7b10
Mo Becher <gitkraken@mo-becher.de>
2018-04-07 15:09:53 +0200

    reuse http connection

--- main.go
+++ main.go
@@ -3,13 +3,38 @@ package main
 // import "fmt"
 import (
 	"VolumioLCD/volumio"
+	"fmt"
 	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	maxIdleConnections int = 20
+	timeout            int = 10
+	updateInterval     int = 200
 )
 
 func main() {
-	state, err := volumio.GetPlayerState()
-	if err != nil {
-		log.Fatal(err)
+	// Create the http client
+	httpClient := http.Client{
+		Transport: &http.Transport{
+			MaxIdleConnsPerHost: maxIdleConnections,
+		},
+		Timeout: time.Duration(timeout) * time.Second,
 	}
-	print(state.Service)
+
+	// Initialize volumio client
+	volumio.URI = "http://volumio.local:3000"
+	volumio.HttpClient = &httpClient
+
+	for true {
+		state, err := volumio.GetPlayerState()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("\r%s (%s)", state.Title, state.Artist)
+
+		time.Sleep(time.Duration(updateInterval) * time.Millisecond)
+	}
 }
--- volumio/volumio.go
+++ volumio/volumio.go
@@ -5,6 +5,20 @@ import (
 	"net/http"
 )
 
+var (
+	HttpClient *http.Client
+	URI        string
+)
+
+const (
+	maxIdleConnections int = 20
+	requestTimeout     int = 10
+)
+
+func init() {
+}
+
+// PlayerState contains all the fields from a volumio REST API 'getstate' call
 type PlayerState struct {
 	Status            string
 	Title             string
@@ -32,13 +46,26 @@ type PlayerState struct {
 	Disableuicontrols bool
 }
 
+// GetPlayerState retrieves player info by calling volumios REST api. The URI of the volumio server must be provided
 func GetPlayerState() (PlayerState, error) {
-	resp, err := http.Get("http://volumio.local:3000/api/v1/getstate")
 	var state PlayerState
+
+	// create the request
+	// FUTURE WORK: reuse request
+	reqURI := URI + "/api/v1/getstate"
+	req, err := http.NewRequest("GET", reqURI, nil)
+	if err != nil {
+		return state, err
+	}
+
+	// send the request with the reused http connection
+	resp, err := HttpClient.Do(req)
 	if err != nil {
 		return state, err
 	}
 	defer resp.Body.Close()
+
+	// decode the json response
 	err = json.NewDecoder(resp.Body).Decode(&state)
 	if err != nil {
 		return state, err
